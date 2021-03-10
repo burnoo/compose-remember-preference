4191df627d36169877cf0a11f98f0a770588acb3
burno <bruno.wieczorek@gmail.com>
2021-03-10 19:00:24 +0100

    Update example

--- sample/src/main/java/dev/burnoo/compose/rememberpreference/sample/MainActivity.kt
+++ sample/src/main/java/dev/burnoo/compose/rememberpreference/sample/MainActivity.kt
@@ -3,35 +3,76 @@ package dev.burnoo.compose.rememberpreference.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.burnoo.compose.rememberpreference.sample.ui.theme.ComposerememberpreferenceTheme
 import dev.burnoo.compose.rememberpreferences.rememberIntPreference
+import dev.burnoo.compose.rememberpreferences.rememberStringPreference
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposerememberpreferenceTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Clicker()
+                    App()
                 }
             }
         }
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
-fun Clicker() {
-    var count by rememberIntPreference(keyName = "countKey", initialValue = 0, defaultValue = 0)
+fun App() {
+    var count by rememberIntPreference(
+        keyName = "countKey",
+        initialValue = 0,
+        defaultValue = 0
+    )
+    val (text, setText) = rememberStringPreference(keyName = "textKey", defaultValue = "Hello!")
 
-    Button(onClick = { count++ }) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Clicker(count, onClick = { count++ })
+        AnimatedVisibility(visible = text != null) {
+            TextField(
+                value = text.orEmpty(),
+                onValueChange = setText,
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun Clicker(count: Int = 0, onClick: () -> Unit = {}) {
+    Button(onClick = onClick) {
         Text(text = count.toString())
     }
 }
