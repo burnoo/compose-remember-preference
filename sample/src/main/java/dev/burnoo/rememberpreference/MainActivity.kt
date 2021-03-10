921e7d2e5f5fe9db56b0f63444bc7ed080c9883b
burno <bruno.wieczorek@gmail.com>
2021-03-10 17:36:16 +0100

    Initial commit

---
+++ sample/src/main/java/dev/burnoo/rememberpreference/MainActivity.kt
@@ -0,0 +1,38 @@
+package dev.burnoo.rememberpreference
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.burnoo.rememberpreference.ui.theme.ComposerememberpreferenceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposerememberpreferenceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposerememberpreferenceTheme {
+        Greeting("Android")
+    }
+}
